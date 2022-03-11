ccc0d4d6b399269a484b6d48e27ce4ac1b630799
Paweł Nguyen <pawel.nguyen@gmail.com>
2022-03-11 11:37:11 +0100

    WIP simple operation modal for registering account

--- src/renderer/modals/index.js
+++ src/renderer/modals/index.js
@@ -71,6 +71,7 @@ import MODAL_SOLANA_DELEGATION_WITHDRAW from "../families/solana/DelegationWithd
 import MODAL_CELO_MANAGE from "../families/celo/ManageModal";
 import MODAL_CELO_LOCK from "../families/celo/LockFlowModal";
 import MODAL_CELO_UNLOCK from "../families/celo/UnlockFlowModal";
+import MODAL_CELO_SIMPLE_OPERATION from "../families/celo/SimpleOperationFlowModal";
 
 // Lending
 import MODAL_LEND_MANAGE from "../screens/lend/modals/ManageLend";
@@ -140,6 +141,7 @@ const modals: { [_: string]: React$ComponentType<any> } = {
   MODAL_CELO_MANAGE,
   MODAL_CELO_LOCK,
   MODAL_CELO_UNLOCK,
+  MODAL_CELO_SIMPLE_OPERATION,
   MODAL_FULL_NODE,
   MODAL_LOTTIE_DEBUGGER,
   MODAL_RECOVERY_SEED_WARNING,
