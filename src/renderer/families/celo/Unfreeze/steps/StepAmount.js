6ae726cae9912862050c081819b4b6b7fb2adfc8
Paweł Nguyen <pawel.nguyen@gmail.com>
2022-04-04 10:26:39 +0100

    Display withdrawal time, enable/disable selects, display fees

--- src/renderer/families/celo/Unfreeze/steps/StepAmount.js
+++ src/renderer/families/celo/Unfreeze/steps/StepAmount.js
@@ -1,6 +1,6 @@
 // @flow
 import invariant from "invariant";
-import React, { useCallback, useMemo } from "react";
+import React, { useCallback } from "react";
 import styled from "styled-components";
 
 import { Trans } from "react-i18next";
@@ -13,16 +13,13 @@ import { getAccountBridge } from "@ledgerhq/live-common/lib/bridge";
 import TrackPage from "~/renderer/analytics/TrackPage";
 import Box from "~/renderer/components/Box";
 import Button from "~/renderer/components/Button";
-import Alert from "~/renderer/components/Alert";
 import Text from "~/renderer/components/Text";
 import FormattedVal from "~/renderer/components/FormattedVal";
 import CheckBox from "~/renderer/components/CheckBox";
 import Clock from "~/renderer/icons/Clock";
+import AccountFooter from "~/renderer/modals/Send/AccountFooter";
 
 import ErrorBanner from "~/renderer/components/ErrorBanner";
-
-import { getUnfreezeData } from "../Body";
-import { BigNumber } from "bignumber.js";
 
 const Description = styled(Text).attrs(({ isPill }) => ({
   ff: isPill ? "Inter|SemiBold" : "Inter|Regular",
@@ -118,31 +115,35 @@ export default function StepAmount({
       <TrackPage category="Unfreeze Flow" name="Step 1" />
       {error ? <ErrorBanner error={error} /> : null}
       <Box vertical>
-        {pendingWithdrawals.map(({ value, time, index }) => (
-          <SelectResource disabled={index % 2} key={index}>
-            <Text ff="Inter|SemiBold"></Text>
-            <Box horizontal alignItems="center">
-              <TimerWrapper>
-                <Clock size={12} />
-                <Description isPill>{time.toString()}</Description>
-              </TimerWrapper>
-              <FormattedVal
-                val={value}
-                unit={account.unit}
-                style={{ textAlign: "right", width: "auto", marginRight: 10 }}
-                showCode
-                fontSize={4}
-                color="palette.text.shade60"
-              />
-              <CheckBox
-                isRadio
-                disabled={index % 2}
-                isChecked={transaction.index === index}
-                onChange={() => onChange(index)}
-              />
-            </Box>
-          </SelectResource>
-        ))}
+        {pendingWithdrawals.map(({ value, time, index }) => {
+          const withdrawalTime = new Date(time.toNumber() * 1000);
+          const disabled = withdrawalTime > new Date();
+          return (
+            <SelectResource disabled={disabled} key={index}>
+              <Text ff="Inter|SemiBold"></Text>
+              <Box horizontal alignItems="center">
+                <TimerWrapper>
+                  <Clock size={12} />
+                  <Description isPill>{moment(withdrawalTime).fromNow()}</Description>
+                </TimerWrapper>
+                <FormattedVal
+                  val={value}
+                  unit={account.unit}
+                  style={{ textAlign: "right", width: "auto", marginRight: 10 }}
+                  showCode
+                  fontSize={4}
+                  color="palette.text.shade60"
+                />
+                <CheckBox
+                  isRadio
+                  disabled={disabled}
+                  isChecked={transaction.index === index}
+                  onChange={() => onChange(index)}
+                />
+              </Box>
+            </SelectResource>
+          );
+        })}
       </Box>
     </Box>
   );
@@ -163,13 +164,16 @@ export function StepAmountFooter({
   const canNext = !bridgePending && !hasErrors;
 
   return (
-    <Box horizontal>
-      <Button mr={1} secondary onClick={onClose}>
-        <Trans i18nKey="common.cancel" />
-      </Button>
-      <Button disabled={!canNext} primary onClick={() => transitionTo("connectDevice")}>
-        <Trans i18nKey="common.continue" />
-      </Button>
-    </Box>
+    <>
+      <AccountFooter parentAccount={parentAccount} account={account} status={status} />
+      <Box horizontal>
+        <Button mr={1} secondary onClick={onClose}>
+          <Trans i18nKey="common.cancel" />
+        </Button>
+        <Button disabled={!canNext} primary onClick={() => transitionTo("connectDevice")}>
+          <Trans i18nKey="common.continue" />
+        </Button>
+      </Box>
+    </>
   );
 }
