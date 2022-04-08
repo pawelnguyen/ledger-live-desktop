aed373b524cc902f3eda50322967164245e4fcf0
Paweł Nguyen <pawel.nguyen@gmail.com>
2022-04-08 13:55:03 +0100

    WIP modals work

--- src/renderer/modals/index.js
+++ src/renderer/modals/index.js
@@ -63,6 +63,7 @@ import MODAL_POLKADOT_REBOND from "../families/polkadot/RebondFlowModal";
 import MODAL_CELO_MANAGE from "../families/celo/ManageModal";
 import MODAL_CELO_LOCK from "../families/celo/LockFlowModal";
 import MODAL_CELO_UNLOCK from "../families/celo/UnlockFlowModal";
+import MODAL_CELO_VOTE from "../families/celo/DelegationFlowModal";
 import MODAL_CELO_SIMPLE_OPERATION from "../families/celo/SimpleOperationFlowModal";
 import MODAL_CELO_WITHDRAW from "../families/celo/WithdrawFlowModal";
 
@@ -127,6 +128,7 @@ const modals: { [_: string]: React$ComponentType<any> } = {
   MODAL_CELO_MANAGE,
   MODAL_CELO_LOCK,
   MODAL_CELO_UNLOCK,
+  MODAL_CELO_VOTE,
   MODAL_CELO_SIMPLE_OPERATION,
   MODAL_CELO_WITHDRAW,
   MODAL_FULL_NODE,
