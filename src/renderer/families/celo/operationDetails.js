2ca8c4be395ad72909502d76721e69067038b6cc
Paweł Nguyen <pawel.nguyen@gmail.com>
2022-05-26 10:18:11 +0200

    Display validators in vote/revoke/activate operation details

--- src/renderer/families/celo/operationDetails.js
+++ src/renderer/families/celo/operationDetails.js
@@ -1,91 +1,94 @@
-/* eslint-disable consistent-return */
 // @flow
 import React from "react";
 
-import { BigNumber } from "bignumber.js";
-
-import type { Currency, Unit, Operation, Account } from "@ledgerhq/live-common/lib/types";
+import type { Currency, Operation, Account } from "@ledgerhq/live-common/lib/types";
 
 import {
   OpDetailsTitle,
-  OpDetailsData,
   OpDetailsSection,
+  OpDetailsData,
+  OpDetailsVoteData,
+  Address,
 } from "~/renderer/drawers/OperationDetails/styledComponents";
 import { Trans } from "react-i18next";
-import Box from "~/renderer/components/Box/Box";
+import { useCeloPreloadData } from "@ledgerhq/live-common/lib/families/celo/react";
+import Box from "~/renderer/components/Box";
+import Text from "~/renderer/components/Text";
+import { SplitAddress } from "~/renderer/components/OperationsList/AddressCell";
+import { getAddressExplorer, getDefaultExplorerView } from "@ledgerhq/live-common/lib/explorers";
+import { openURL } from "~/renderer/linking";
 import FormattedVal from "~/renderer/components/FormattedVal";
-import CounterValue from "~/renderer/components/CounterValue";
+
+const redirectAddress = (currency: Currency, address: string) => () => {
+  const url = getAddressExplorer(getDefaultExplorerView(currency), address);
+  if (url) openURL(url);
+};
 
 type OperationDetailsExtraProps = {
+  operation: Operation,
   extra: { [key: string]: any },
   type: string,
   account: Account,
 };
 
-const OperationDetailsExtra = ({ extra, type, account }: OperationDetailsExtraProps) => {
+const OperationDetailsExtra = ({ operation, type, account }: OperationDetailsExtraProps) => {
+  const { currency } = account;
+  const { validatorGroups } = useCeloPreloadData();
+
   switch (type) {
-    case "VOTE":
+    case "ACTIVATE":
+    case "REVOKE":
+    case "VOTE": {
+      const recipient = operation.recipients[0];
+      const validatorGroup =
+        recipient &&
+        validatorGroups.find(
+          validatorGroup => validatorGroup.address.toLowerCase() === recipient.toLowerCase(),
+        );
+
       return (
-        <OpDetailsSection>
-          <OpDetailsTitle>
-            <Trans i18nKey="operationDetails.extra.unfreezeAmount" />
-          </OpDetailsTitle>
-          <OpDetailsData>
-            <Box>
-              <FormattedVal
-                val={extra.unfreezeAmount}
-                unit={account.unit}
-                showCode
-                fontSize={4}
-                color="palette.text.shade60"
-              />
+        <>
+          <OpDetailsSection>
+            <OpDetailsTitle>
+              <Trans i18nKey={`operation.type.${type}`} />
+            </OpDetailsTitle>
+            <OpDetailsData>
+              <Box>
+                <FormattedVal
+                  val={operation.value}
+                  unit={account.unit}
+                  showCode
+                  fontSize={4}
+                  color="palette.text.shade60"
+                />
+              </Box>
+            </OpDetailsData>
+          </OpDetailsSection>
+          <OpDetailsSection>
+            <OpDetailsTitle>
+              <Trans i18nKey={"celo.delegation.validatorGroup"} />
+            </OpDetailsTitle>
+            <Box flex="1" pl={2}>
+              <OpDetailsData justifyContent="flex-start">
+                <OpDetailsVoteData>
+                  <Box>
+                    <Text ff="Inter|SemiBold">{validatorGroup?.name}</Text>
+                  </Box>
+                  <Address onClick={redirectAddress(currency, validatorGroup?.address)}>
+                    <SplitAddress value={validatorGroup?.address} />
+                  </Address>
+                </OpDetailsVoteData>
+              </OpDetailsData>
             </Box>
-          </OpDetailsData>
-        </OpDetailsSection>
+          </OpDetailsSection>
+        </>
       );
+    }
     default:
       return null;
   }
 };
 
-type Props = {
-  operation: Operation,
-  currency: Currency,
-  unit: Unit,
-};
-
-const AmountCell = ({ operation, currency, unit }: Props) => {
-  const amount = new BigNumber(operation.value);
-
-  return (
-    !amount.isZero() && (
-      <>
-        <FormattedVal
-          val={amount}
-          unit={unit}
-          showCode
-          fontSize={4}
-          color={"palette.text.shade80"}
-        />
-
-        <CounterValue
-          color="palette.text.shade60"
-          fontSize={3}
-          date={operation.date}
-          currency={currency}
-          value={amount}
-        />
-      </>
-    )
-  );
-};
-
-const amountCellExtra = {
-  UNLOCK: AmountCell,
-  VOTE: AmountCell,
-};
-
 export default {
   OperationDetailsExtra,
-  amountCellExtra,
 };
